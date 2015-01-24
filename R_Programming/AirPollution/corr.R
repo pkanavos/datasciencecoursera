e242006c06aeadda7b91a18fbd766913ae616c0c
Panagiotis Kanavos <pkanavos@gmail.com>
2015-01-24 23:40:23 +0200

    Finish corr

--- R_Programming/AirPollution/corr.R
+++ R_Programming/AirPollution/corr.R
@@ -8,5 +8,12 @@ corr <- function(directory, threshold = 0) {
         ## nitrate and sulfate; the default is 0
 
         ## Return a numeric vector of correlations
+
         allNobs<-complete(directory)
+
+        ids<-allNobs[allNobs$nobs>threshold,"id"]
+        files<-sprintf("%s/%03d.csv",directory,ids)
+
+        correlations<-lapply(files,function(x) cor(read.csv(x)[c(2,3)],use="na.or.complete")["sulfate","nitrate"])
+		unlist(correlations)
 }
